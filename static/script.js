898b751646f3959e82721d9f57c3b988aee04b52
nawnex <117183583+nawnex@users.noreply.github.com>
2023-04-20 02:17:34 +0200

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,81 @@
+const chat = document.getElementById('chat');
+const userInput = document.getElementById('user-input');
+let conversation = [];
+
+
+userInput.addEventListener('keypress', handleKeyPress);
+
+function handleKeyPress(event) {
+    if (event.keyCode === 13) {
+    event.preventDefault();
+    sendMessage();
+    }
+}
+
+function createMessage(role, content) {
+    const message = document.createElement('div');
+    message.className = 'message ' + role + '-message';
+    message.textContent = content;
+    return message;
+}
+
+function displayMessage(role, content) {
+    const message = createMessage(role, content);
+    chat.appendChild(message);
+    chat.scrollTop = chat.scrollHeight;
+}
+
+
+
+function sendMessage() {
+    var message = userInput.value;
+    userInput.value = "";
+
+    const userMessage = createMessage('user', message);
+    chat.appendChild(userMessage);
+    chat.scrollTop = chat.scrollHeight;
+
+    // Display typing dots
+    const typingDots = createMessage('bot', '...');
+    chat.appendChild(typingDots);
+    chat.scrollTop = chat.scrollHeight;
+
+    // Generate a unique user ID or retrieve an existing one
+    var userId = window.sessionStorage.getItem('userId');
+    if (!userId) {
+        userId = Math.random().toString(36).substr(2, 9);
+        window.sessionStorage.setItem('userId', userId);
+    }
+
+    // Send the user message and ID to the server
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(this.responseText);
+
+            // Remove typing dots and display bot's response
+            typingDots.remove();
+            displayMessage('bot', response.answer);
+        }
+    };
+    xmlhttp.open("POST", "/chat", true);
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlhttp.send(JSON.stringify({ user_id: userId, user_input: message }));
+}
+
+
+
+function toggleChat() {
+    var chat = document.getElementById("chat");
+    var chatbox = document.querySelector(".container");
+    var minimize = document.getElementById("minimized-chat");
+    if (chatbox.style.display === "none") {
+        chatbox.style.display = "flex";
+        minimize.style.display = "none";
+    } else {
+        chatbox.style.display = "none";
+        minimize.style.display = "flex";
+    }
+    chat.scrollTop = chat.scrollHeight;
+    document.getElementById("user-input").focus();
+}
